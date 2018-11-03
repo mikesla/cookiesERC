cdb69fba6c02438c684a2a2066a0bde574202991
Mikhail <mroopver@gmail.com>
2018-11-03 17:19:33 +0300

    Initial commit

---
+++ ERC42.sol
@@ -0,0 +1,65 @@
+pragma solidity ^0.4.18;
+
+import "./zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
+
+/**
+ * @title ERC865Token Token
+ *
+ * ERC865Token allows users paying transfers in tokens instead of gas
+ * https://github.com/ethereum/EIPs/issues/865
+ *
+ */
+
+contract ERC865 is ERC721 {
+
+    function transferPreSigned(
+        bytes _signature,
+        address _to,
+        uint256 _value,
+        uint256 _fee,
+        uint256 _nonce
+    )
+        public
+        returns (bool);
+
+    function approvePreSigned(
+        bytes _signature,
+        address _spender,
+        uint256 _value,
+        uint256 _fee,
+        uint256 _nonce
+    )
+        public
+        returns (bool);
+
+    function increaseApprovalPreSigned(
+        bytes _signature,
+        address _spender,
+        uint256 _addedValue,
+        uint256 _fee,
+        uint256 _nonce
+    )
+        public
+        returns (bool);
+
+    function decreaseApprovalPreSigned(
+        bytes _signature,
+        address _spender,
+        uint256 _subtractedValue,
+        uint256 _fee,
+        uint256 _nonce
+    )
+        public
+        returns (bool);
+
+    function transferFromPreSigned(
+        bytes _signature,
+        address _from,
+        address _to,
+        uint256 _value,
+        uint256 _fee,
+        uint256 _nonce
+    )
+        public
+        returns (bool);
+}
